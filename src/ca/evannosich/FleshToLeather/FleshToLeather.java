941a7cbe69fe762ce2c7b31a1dea0758693f8271
Evan Nosich <evannosich@icloud.com>
2019-11-23 13:23:31 -0500

    Flesh To Leather 1.0

---
+++ src/ca/evannosich/FleshToLeather/FleshToLeather.java
@@ -0,0 +1,33 @@
+package ca.evannosich.FleshToLeather;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FleshToLeather extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+        ItemStack leatherItem = new ItemStack(Material.LEATHER);
+
+        NamespacedKey leatherKey = new NamespacedKey(this, "LEATHER");
+
+        float experience = 0.35f;
+
+        int cookingTime = 200;
+
+        FurnaceRecipe leatherRecipe = new FurnaceRecipe(leatherKey, leatherItem, Material.ROTTEN_FLESH, experience, cookingTime);
+
+        Bukkit.addRecipe(leatherRecipe);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
