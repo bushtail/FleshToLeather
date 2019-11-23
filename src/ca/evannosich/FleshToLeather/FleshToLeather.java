be6d259ab65302c23f7915041a65adb122873bf0
Evan Nosich <evannosich@icloud.com>
2019-11-23 14:42:27 -0500

    formatting edit

--- src/ca/evannosich/FleshToLeather/FleshToLeather.java
+++ src/ca/evannosich/FleshToLeather/FleshToLeather.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FleshToLeather extends JavaPlugin {
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
 
         ItemStack leatherItem = new ItemStack(Material.LEATHER);
 
